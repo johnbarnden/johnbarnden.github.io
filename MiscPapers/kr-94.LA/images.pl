3ffaa257365cb2f5428c6f61144c43554204b8db
johnbarnden <jabarnden@btinternet.com>
2024-11-14 18:35:20 +0000

    Add files via upload

---
+++ MiscPapers/kr-94.LA/images.pl
@@ -0,0 +1,52 @@
+# LaTeX2HTML 95 (Thu Jan 19 1995)
+# Associate image original text (scrambled) with physical files.
+
+$key = q/{_inline}$rho_0${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img57.gif">'; 
+$key = q/{_inline}$tau${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img17.gif">'; 
+$key = q/{_inline}$_1${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img93.gif">'; 
+$key = q/{_inline}$chi${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img27.gif">'; 
+$key = q/{_inline}$rho_n${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img97.gif">'; 
+$key = q/{_inline}$mu${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img26.gif">'; 
+$key = q/{_inline}$land${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img16.gif">'; 
+$key = q/{_inline}$rho;SPM_gt;rho^primege${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img120.gif">'; 
+$key = q/{_inline}$}${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img47.gif">'; 
+$key = q/{_inline}$ldots${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img94.gif">'; 
+$key = q/{_inline}$setminus${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img40.gif">'; 
+$key = q/{_inline}$rho^prime${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img72.gif">'; 
+$key = q/{_inline}$overlinermQ${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img82.gif">'; 
+$key = q/{_inline}$overlinermP${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img58.gif">'; 
+$key = q/{_inline}$rho${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img125.gif">'; 
+$key = q/{_inline}$_n${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img102.gif">'; 
+$key = q/{_inline}$rho_1${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img113.gif">'; 
+$key = q/{_inline}$^prime.Q${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img150.gif">'; 
+$key = q/{_inline}$overlineB${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img128.gif">'; 
+$key = q/{_inline}$longrightarrow${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img24.gif">'; 
+$key = q/{_inline}$_i${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img104.gif">'; 
+$key = q/{_inline}${${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img52.gif">'; 
+$key = q/{_inline}$^prime${_inline}/;
+$cached_env_img{$key} ='<IMG  ALIGN=BOTTOM ALT="" SRC="img7.gif">'; 
+
+1;
+
